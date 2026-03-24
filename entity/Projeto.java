65e26dfdea1d573ddb7eeede526c342cf0998a36
JoaoGMB <muller.bordin@gmail.com>
2026-03-24 17:02:11 -0300

    Add files via upload

---
+++ entity/Projeto.java
@@ -0,0 +1,52 @@
+package com.example.projeto.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Projeto")
+@Table(name = "tb_projeto")
+public class Projeto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_projeto")
+    private String nome;
+
+    @Column(name = "data_inicio")
+    private LocalDate dataInicio;
+
+    @Column(name = "data_fim")
+    private LocalDate dataFim;
+
+    public Projeto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
